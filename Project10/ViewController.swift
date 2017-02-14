c39060645e75296ec09196af0af0dfbe52a7c32c
mradzwilla <mradzwilla@gmail.com>
2017-02-13 23:57:38 -0500

    Initial Commit

---
+++ Project10/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Project10
+//
+//  Created by Michael Radzwilla on 2/13/17.
+//  Copyright © 2017 swifttutorial. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
